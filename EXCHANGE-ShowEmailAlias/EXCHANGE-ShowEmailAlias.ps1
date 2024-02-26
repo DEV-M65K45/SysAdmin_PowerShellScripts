9df3a4d9bb2af497980eca6af1804a6ac5749d6c
DEV-M65K45 <159099974+DEV-M65K45@users.noreply.github.com>
2024-02-26 12:36:01 -0600

    update scripts and paths

--- EXCHANGE-ShowEmailAlias/EXCHANGE-ShowEmailAlias.ps1
+++ EXCHANGE-ShowEmailAlias/EXCHANGE-ShowEmailAlias.ps1
@@ -1,1 +1,24 @@
+<#
+.SYNOPSIS
+    Gets all email alias and saves then to a csv file for manipulation/searching.
+.DESCRIPTION
+    This script gets all email alias' in the domain and puts them in a csv file for manipulation/searching/etc. 
+.PARAMETER N/A
+    No parameters needed
+.EXAMPLE
+    Run script as is listed below.
+.NOTES
+    NAME:    EXCHANGE-ShowEmailAlias.ps1
+    AUTHOR:    Lassalle
+    DATE:    2024/2/26
+    WWW:    
+    GITHUB: https://github.com/DEV-M65K45
 
+    VERSION HISTORY:
+    2024.1
+        Initial Version
+#>
+
+
+$csvFile = "C:\temp\aliaslist.csv"
+Get-Mailbox | Select-Object DisplayName,@{Name=”EmailAddresses”;Expression={$_.EmailAddresses |Where-Object {$_ -LIKE “SMTP:*”}}} | Sort | Export-Csv $csvFile
