aa221a01e818d00099e4dc1b6357049c510e5d57
DEV-M65K45 <159099974+DEV-M65K45@users.noreply.github.com>
2024-04-21 00:14:52 -0500

    Update EXCHANGE-EditDistributionGroups.ps1

--- EXCHANGE-EditDistributionGroups/EXCHANGE-EditDistributionGroups.ps1
+++ EXCHANGE-EditDistributionGroups/EXCHANGE-EditDistributionGroups.ps1
@@ -8,7 +8,7 @@
 .EXAMPLE
     Run command(s) as listed below.
 .NOTES
-    NAME:    WINDOWS-EditDistributionGroupSettings.ps1
+    NAME:    EXCHANGE-EditDistributionGroupSettings.ps1
     AUTHOR:    Lassalle
     DATE:    2024/2/26
     WWW:    
@@ -21,19 +21,19 @@
 
 
 #look at all settings on a dynamic distribution group
-$groupName = _GRP.TestGroup@domain.com   
+$groupName = TestGroup@domain.com   
 Get-DynamicDistributionGroup -Identity $groupName | Format-List
 
 #look at specific settings on a dynamic distribution group.  Choose from results of list above.  "-RequireSenderAuthenticatedEnabled" is only an example
-$groupName = _GRP.TestGroup@domain.com    
+$groupName = TestGroup@domain.com    
 Get-DynamicDistributionGroup -Identity $groupName | Select-Object RequireSenderAuthenticationEnabled
 
 #set settings on a dynamic distribution group.  Choose from results of list above.  "-RequireSenderAuthenticatedEnabled" is only an example.
-$groupName = _GRP.TestGroup@domain.com    
+$groupName = TestGroup@domain.com    
 Set-DynamicDistributionGroup -Identity $groupName -RequireSenderAuthenticationEnabled $false
 
 #bulk edit distribution groups.  Choose from results of list above.  "-RequireSenderAuthenticatedEnabled" is only an example.
-$groupName = _GRP.TestGroup@domain.com 
+$groupName = TestGroup@domain.com 
 #csv file containing list of distribution groups to edit.
 $csvFile = "C:\temp\csvfile.csv"
 Get-Content $csvFile | Set-DynamicDistributionGroup -RequireSenderAuthenticationEnabled $false
